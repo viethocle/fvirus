601d8c95ff3ddd19229fad6bfd895c334ade7da7
minhthuan <doanminhthuan274@gmail.com>
2018-01-12 10:58:23 +0700

    add html footer

--- src/app/modules/layout/footer/footer.component.ts
+++ src/app/modules/layout/footer/footer.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+// import { TranslateService } from "@ngx-translate/core";
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  selector: "[footer]",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.css"]
 })
 export class FooterComponent implements OnInit {
+  langs: [{ name: "English"; lang: "en" }, { name: "Tiếng Việt"; lang: "vi" }];
+  defaultValue = "en";
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    // this.defaultValue = this.translate.getDefaultLang();
   }
 
+  selectCountry(lang: string) {
+    // this.translate.use(lang);
+    localStorage.setItem("lang", lang);
+    window.location.reload();
+  }
 }
