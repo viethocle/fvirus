90d5cbc6262a977a3d180cc76e8e0b6df2c04864
minhthuan <doanminhthuan274@gmail.com>
2018-01-12 14:07:58 +0700

    add bs modal and test

--- src/app/modules/dashboard/components/create-order/create-order.component.ts
+++ src/app/modules/dashboard/components/create-order/create-order.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { InputFieldComponent } from '@shared/components';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { BsModalComponent } from 'ng2-bs3-modal';
 @Component({
   selector: 'app-create-order',
   templateUrl: './create-order.component.html',
   styleUrls: ['./create-order.component.css']
 })
 export class CreateOrderComponent implements OnInit {
+
+  @ViewChild("modalCreate") modalCreate: BsModalComponent;
+
 
   constructor() { }
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,17 +3,26 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { InputFieldComponent } from './components/';
 import { ClickOutsideModule } from "ng-click-outside";
+import { BsModalModule }       from "ng2-bs3-modal";
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
+  declarations: [
+    InputFieldComponent
+  ],
   imports: [
-    CommonModule,
     FormsModule,
+    CommonModule,
     ReactiveFormsModule,
-    ClickOutsideModule
+    ClickOutsideModule,
+    BsModalModule,
+    TranslateModule,
   ],
   exports: [
-    ClickOutsideModule
+    ClickOutsideModule,
+    InputFieldComponent,
+    BsModalModule,
+    TranslateModule
   ],
-  declarations: [InputFieldComponent]
 })
 export class SharedModule { }
