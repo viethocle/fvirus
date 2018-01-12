c1c7ee5733c8064f4cf6edd6726bbd38b3376e61
minhthuan <doanminhthuan274@gmail.com>
2018-01-12 09:30:27 +0700

    import module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
 
 import { AppRoutingModule } from './app.routing';
 
@@ -12,6 +15,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CreateOrderComponent } from './components/create-order/create-order.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CreateOrderComponent]
+})
+export class DashboardModule { }
