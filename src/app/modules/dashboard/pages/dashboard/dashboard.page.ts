d18beb08a2b071bcd8b735dc5a9c54c5325aab65
minhthuan <doanminhthuan274@gmail.com>
2018-01-15 11:26:32 +0700

    fix page

--- src/app/modules/dashboard/pages/dashboard/dashboard.page.ts
+++ src/app/modules/dashboard/pages/dashboard/dashboard.page.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  templateUrl: './dashboard.page.html',
+  styleUrls: ['./dashboard.page.css']
 })
 export class DashboardPage implements OnInit {
 
