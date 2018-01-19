9c090bb002cf820737e6de41fd77215f5f955aee
minhthuan <doanminhthuan274@gmail.com>
2018-01-19 09:36:00 +0700

    add order tables page

---
+++ src/app/modules/dashboard/pages/table-orders/table-orders.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-table-orders',
+  templateUrl: './table-orders.page.html',
+  styleUrls: ['./table-orders.page.css']
+})
+export class TableOrdersPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
