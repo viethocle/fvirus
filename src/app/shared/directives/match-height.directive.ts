f8a930df8e26708fa8d5da6ed8cc141b5c8a92ef
minhthuan <doanminhthuan274@gmail.com>
2018-02-07 13:46:41 +0700

    set all children heights equal

--- src/app/shared/directives/match-height.directive.ts
+++ src/app/shared/directives/match-height.directive.ts
@@ -6,8 +6,37 @@ import {
 @Directive({
   selector: '[appMatchHeight]'
 })
-export class MatchHeightDirective {
+export class MatchHeightDirective implements AfterViewChecked{
 
-  constructor() { }
+  @Input()
+  myMatchHeight: string;
+
+  constructor(private el: ElementRef) { 
+
+  }
+
+  ngAfterViewChecked() {
+    this.matchHeight(this.el.nativeElement, this.myMatchHeight);
+  }
+
+  matchHeight(parent: HTMLElement, className: string) {
+    
+    if (!parent) return;
+
+    const children = parent.getElementsByClassName(className);
+    if (!children) return;
+
+    // Get all child elements heights
+    const itemHeights = Array.from(children)
+                             .map(x => x.getBoundingClientRect().height);
+
+    // find out the tallest
+    const maxHeight = itemHeights.reduce((prev, curr) => {
+      return curr > prev ? curr : prev;
+    }, 0);
+
+    Array.from(children)
+         .forEach((x: HTMLElement) => x.style.height = `${maxHeight}px`);
+  }
 
 }
