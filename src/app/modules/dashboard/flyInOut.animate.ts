9881dfb9ac42b2f067eb831f5ce2bc53444b20b4
thuan274 <doanminhthuan274@gmail.com>
2018-02-25 11:32:32 +0700

    chaneg animate

--- src/app/modules/dashboard/flyInOut.animate.ts
+++ src/app/modules/dashboard/flyInOut.animate.ts
@@ -5,8 +5,8 @@ export const FlyInOut: AnimationEntryMetadata  =
   trigger('flyInOut', [
     state('in', style({transform: 'translateX(0)'})),
     transition('void => *', [
-      style({transform: 'translateX(-100%)'}),
-      animate(100)
+      style({transform: 'translateX(-80%)'}),
+      animate(130)
     ]),
     transition('* => void', [
       animate(100, style({transform: 'translateX(100%)'}))
