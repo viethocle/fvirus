8793f0359a04b4eb911af04f1c96e4ab88b7ffd1
minhthuan <doanminhthuan274@gmail.com>
2018-02-28 20:52:56 +0700

    add build forM

--- src/app/modules/dashboard/components/edit-order/edit-order.component.ts
+++ src/app/modules/dashboard/components/edit-order/edit-order.component.ts
@@ -1,4 +1,14 @@
+import { Order } from './../../order';
+import { BsmodalService } from './../../bsmodal.service';
+import { DashboardService } from './../../dashboard.service';
 import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormBuilder,
+  AbstractControl
+} from '@angular/forms';
 
 @Component({
   selector: 'app-edit-order',
@@ -7,9 +17,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditOrderComponent implements OnInit {
 
-  constructor() { }
+  order: Order;
+  formEditOrder: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private dashboardService: DashboardService,
+    private bsmodalService: BsmodalService
+  ) { }
 
   ngOnInit() {
+    this.buildForm();
+  }
+
+  private buildForm() {
+    this.formEditOrder = this.formBuilder.group({
+      description: [""],
+      due_date: ["", Validators.required],
+      customer_id: [""]
+    });
+
   }
 
 }
