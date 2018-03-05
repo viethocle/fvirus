c270a99376732bc55b38c6f1c4ebc3e3bc954a56
thuan274 <doanminhthuan274@gmail.com>
2018-03-05 10:09:12 +0700

    add new component create customer

--- src/app/modules/customer/customer.module.ts
+++ src/app/modules/customer/customer.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from "@angular/router";
 import { CustomerComponent } from './pages/index-customers/customer.component';
 import { SharedModule } from "@shared/shared.module";
 import { EditCustomerComponent } from './components/edit-customer/edit-customer.component';
+import { CreateCustomerComponent } from './components/create-customer/create-customer.component';
 export const CustomerRoutes: Routes = [
   {
     path: "",
@@ -17,7 +18,7 @@ export const CustomerRoutes: Routes = [
     SharedModule,
     RouterModule.forChild(CustomerRoutes)
   ],
-  declarations: [CustomerComponent, EditCustomerComponent]
+  declarations: [CustomerComponent, EditCustomerComponent, CreateCustomerComponent]
 })
 
 export class CustomerModule { }
