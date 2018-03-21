abacf73ff04bb8eb88407cd3420c04bd0c46faf5
Ha Binh <lethihabinh@gmail.com>
2018-03-21 10:55:09 +0700

    layout

---
+++ src/app/modules/dashboard/components/status/status.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { Destroyable, takeUntilDestroy } from "take-until-destroy";
+import { Order, StatusOrder } from "../../order";
+import { DashboardService } from "../../dashboard.service";
+
+@Destroyable
+@Component({
+  selector: "app-status",
+  templateUrl: "./status.component.html",
+  styleUrls: ["./status.component.css"]
+})
+export class StatusComponent implements OnInit {
+  @Input() order: Order;
+  @Output() statusOrderOutput = new EventEmitter<Order>();
+  statusOrder = StatusOrder;
+
+  constructor(private dashboardService: DashboardService) {}
+
+  ngOnInit() {}
+
+  updateStatus(order: Order, status_to_change) {
+    const order_id = order.id;
+    // call api to change status order
+    this.dashboardService
+      .updateStatusOrder(order_id, status_to_change)
+      .pipe(
+          takeUntilDestroy(this)
+        )
+      .subscribe(ord => this.statusOrderOutput.emit(order));
+    //   .subscribe((or: Order) => {
+    //     _.assign(this.orders.find(t => t.id === order.id), order);
+    //   });
+  }
+}
