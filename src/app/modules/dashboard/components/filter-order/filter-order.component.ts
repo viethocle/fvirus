d218b5ba16987dc297dddc0fb71d3e0cceda5809
minhthuan <doanminhthuan274@gmail.com>
2018-03-23 15:00:00 +0700

    add sorted by

--- src/app/modules/dashboard/components/filter-order/filter-order.component.ts
+++ src/app/modules/dashboard/components/filter-order/filter-order.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-filter-order',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilterOrderComponent implements OnInit {
 
-  constructor() { }
+  sortedBy = [
+    { key: 'Created at (newest first)',  value: 'created_at_desc'},
+    { key: 'Created at (oldest first)',  value: 'created_at_asc'},
+    { key: 'Due Date (newest first)',    value: 'due_date_desc'},
+    { key: 'Due Date (oldest first)',    value: 'due_date_asc'},
+    { key: 'Customer name (a - z)',      value: 'customer_asc'},
+    { key: 'Customer name (z - a)',      value: 'customer_desc'},
+  ]
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.router.navigate(['.'], { relativeTo: this.route, queryParams: { page: 1, per_page: 10 } })
   }
+
+
 
 }
