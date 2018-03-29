3dc3b1043bdd7b112155d285b9464293d61b554b
minhthuan <doanminhthuan274@gmail.com>
2018-03-29 20:31:03 +0700

    add more like datatable :))

--- src/app/shared/components/sortable-column/sortable-column.component.ts
+++ src/app/shared/components/sortable-column/sortable-column.component.ts
@@ -1,6 +1,7 @@
 import { SortTableService } from './../../../core/services/sort-table.service';
 import { Component, OnInit, Input, Output, EventEmitter, OnDestroy, HostListener, HostBinding } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
+import * as classNames from 'classnames';
 
 @Component({
   selector: '[sortable-column]',
@@ -8,8 +9,9 @@ import { Subscription } from 'rxjs/Subscription';
   styleUrls: ['./sortable-column.component.css']
 })
 export class SortableColumnComponent implements OnInit {
-  @HostBinding('class') classes = 'pointer sorting';
+  @HostBinding('class') classes = 'sorting';
 
+  
   constructor(private sortService: SortTableService) { }
 
   @Input('sortable-column')
@@ -23,14 +25,19 @@ export class SortableColumnComponent implements OnInit {
   @HostListener('click')
   sort() {
     this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    this.classes = classNames('sorting_' + this.sortDirection);
     this.sortService.columnSorted({ sortColumn: this.columnName, sortDirection: this.sortDirection });
   }
 
   ngOnInit() {
+    if (this.sortDirection != '') {
+      this.classes = classNames('sorting_' + this.sortDirection);
+    }
     // subscribe to sort changes so we can react when other columns are sorted
     this.columnSortedSubscription = this.sortService.columnSorted$.subscribe(event => {
       // reset this column's sort direction to hide the sort icons
       if (this.columnName != event.sortColumn) {
+        this.classes = 'sorting'
         this.sortDirection = '';
       }
     });
