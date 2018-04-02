efde7f03b8a4eb0184a26bfeb01908a24501922b
viethoc <viethocpro@gmail.com>
2018-04-02 14:35:34 +0700

    error create user

--- src/app/modules/user/components/create-user/create-user.component.ts
+++ src/app/modules/user/components/create-user/create-user.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { BsModalComponent } from 'ng2-bs3-modal';
+import { User } from './../../../user.model';
+import { ToastrService } from './../../../../shared/toastr.service';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { UserService } from './../../user.service';
+import * as _ from 'lodash';
+
 
 @Component({
   selector: 'app-create-user',
@@ -6,10 +13,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-user.component.css']
 })
 export class CreateUserComponent implements OnInit {
-
-  constructor() { }
+  // @ViewChild("modal") modal: BsModalComponent;
+  form: FormGroup;
+  users: User[]= [];
+  role: any;
+  constructor(
+    private usersService: UserService,
+    private fb: FormBuilder,
+    private toastSvc: ToastrService
+  ) {}
 
   ngOnInit() {
+    this.buildForm();
   }
 
+  addUser(value: any) {
+    // console.log(this.role);
+    // const signUpData = {
+    //   email: value.email,
+    //   name: value.name,
+    //   password: value.password,
+    //   password_confirmation: value.password_confirmation,
+    //   role: this.role ? "manager" : "staff"
+    // };
+
+    // this.usersService.addUser(signUpData).subscribe(user => {
+    //   this.users.push(user);
+    //   this.modal.close();
+    //   this.emailJustAdded = value.email;
+    //   this.passwordJustAdded = value.password;
+    //   this.showAlert = true;
+    //   this.form.reset();
+    // });
+  }
+
+  buildForm() {
+    this.form = this.fb.group(
+      {
+        email: [
+          "",
+          Validators.compose([Validators.email])
+        ],
+        name: ["", Validators.required],
+        password: [
+          "",
+          Validators.compose([Validators.required, Validators.minLength(8)])
+        ],
+        password_confirmation: ["", Validators.required],
+        role: [false]
+      },
+      { validator: this.passwordConfirming }
+    );
+  }
+
+  passwordConfirming(c: AbstractControl) {
+    if (c.get("password").value !== c.get("password_confirmation").value) {
+      return { passwordShouldBeMatched: true };
+    }
+    return null;
+  }
 }
