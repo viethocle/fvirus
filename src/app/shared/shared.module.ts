a3a0b8e178a269082c02509cf3b28b1ebc9ab90d
minhthuan <doanminhthuan274@gmail.com>
2018-04-09 11:01:24 +0700

    add numberic directive

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -28,6 +28,7 @@ import { MultiselectDropdownModule } from 'angular-2-dropdown-multiselect';
 import { SortableColumnComponent } from './components/sortable-column/sortable-column.component';
 import { ColorOrderComponent } from './components/color-order/color-order.component';
 import { MyDatePickerModule } from 'mydatepicker';
+import { NumericDirective } from './directives/numeric.directive';
 
 export class CustomOption extends ToastOptions {
   animate = "flyRight"; // you can pass any options to override defaults
@@ -56,7 +57,8 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     TruncateDescriptionPipe,
     VndPipe,
     SortableColumnComponent,
-    ColorOrderComponent
+    ColorOrderComponent,
+    NumericDirective
   ],
   imports: [
     FormsModule,
