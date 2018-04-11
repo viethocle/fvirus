c8afdff49246642c9baa0827d9d2692559012430
minhthuan <doanminhthuan274@gmail.com>
2018-04-11 10:37:34 +0700

    comment module ngx permission

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,7 +46,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     HttpClientModule,
     HttpModule,
     LoadingBarHttpClientModule,
-    NgxPermissionsModule.forRoot(),
+    // NgxPermissionsModule.forRoot(),
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
