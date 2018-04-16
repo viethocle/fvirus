64b419c8835aaa49d2df5d01af23a5af56b06f1b
minhthuan <doanminhthuan274@gmail.com>
2018-04-16 10:29:02 +0700

    remove unused code

--- src/app/modules/home/components/order-due-date/order-due-date.component.ts
+++ src/app/modules/home/components/order-due-date/order-due-date.component.ts
@@ -1,6 +1,5 @@
 import { Order } from '@modules/dashboard/order';
 import { DashboardService } from '@modules/dashboard/dashboard.service';
-import { DueDate } from './../../order-due-date.model';
 import { Component, OnInit } from '@angular/core';
 import { Subject } from "rxjs/Subject";
 import { Observable } from "rxjs/Observable";
