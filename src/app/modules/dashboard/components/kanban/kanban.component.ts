21247557fc8ac612390ff568458c848f6e193c3c
minhthuan <doanminhthuan274@gmail.com>
2018-04-17 21:18:34 +0700

    delete order live udpate

--- src/app/modules/dashboard/components/kanban/kanban.component.ts
+++ src/app/modules/dashboard/components/kanban/kanban.component.ts
@@ -73,6 +73,9 @@ export class KanbanComponent implements OnInit, OnDestroy {
       }
       if (dataOrder.method == "DELETE") {
         this.orders.filter(order => order.id === dataOrder.data.id);
+        $(".card-kanban").filter(function () {
+          return $(this).data("id") == dataOrder.data.id;
+        }).remove();
       }
     });
   }
