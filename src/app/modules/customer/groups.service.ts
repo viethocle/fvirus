8bdf7ae27eb70f94268f461ece89dbf88067da63
minhthuan <doanminhthuan274@gmail.com>
2018-04-24 20:11:08 +0700

    get all groups

--- src/app/modules/customer/groups.service.ts
+++ src/app/modules/customer/groups.service.ts
@@ -1,8 +1,24 @@
+import { Group } from './group.model';
+import { Observable } from 'rxjs/Observable';
+import { environment } from '@environments/environment';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class GroupsService {
 
-  constructor() { }
+  private baseUrl = environment.baseUrl;
+  private url     = `${this.baseUrl}/groups`;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getGroups(): Observable<Group[]> {
+    const getUrl = this.url + '.json';
+    return this.http
+                .get(getUrl)
+                .map((res: any) => res.groups as Group[]);
+  }
 
 }
