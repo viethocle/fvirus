5fc18b9f44acd838a22af16c4b1f940724001bc2
minhthuan <doanminhthuan274@gmail.com>
2018-04-24 20:07:53 +0700

    add service and page index group

--- src/app/modules/customer/customer.module.ts
+++ src/app/modules/customer/customer.module.ts
@@ -1,3 +1,4 @@
+import { DataTablesModule } from 'angular-datatables';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from "@angular/router";
@@ -5,7 +6,12 @@ import { CustomerComponent } from './pages/index-customers/customer.component';
 import { SharedModule } from "@shared/shared.module";
 import { EditCustomerComponent } from './components/edit-customer/edit-customer.component';
 import { CreateCustomerComponent } from './components/create-customer/create-customer.component';
+import { IndexGroupsComponent } from './pages/index-groups/index-groups.component';
 export const CustomerRoutes: Routes = [
+  {
+    path: "groups",
+    component: IndexGroupsComponent
+  },
   {
     path: "",
     component: CustomerComponent
@@ -16,9 +22,10 @@ export const CustomerRoutes: Routes = [
   imports: [
     CommonModule,
     SharedModule,
-    RouterModule.forChild(CustomerRoutes)
+    RouterModule.forChild(CustomerRoutes),
+    DataTablesModule
   ],
-  declarations: [CustomerComponent, EditCustomerComponent, CreateCustomerComponent]
+  declarations: [CustomerComponent, EditCustomerComponent, CreateCustomerComponent, IndexGroupsComponent]
 })
 
 export class CustomerModule { }
---
+++ src/app/modules/customer/pages/index-groups/index-groups.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-index-groups',
+  templateUrl: './index-groups.component.html',
+  styleUrls: ['./index-groups.component.css']
+})
+export class IndexGroupsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
