28ae9cde78c33c8227c7e7dbda07f31aecf3b108
minhthuan <doanminhthuan274@gmail.com>
2018-05-02 20:10:54 +0700

    add method form in comp

--- src/app/modules/customer/components/customers/form-customer/form-customer.component.ts
+++ src/app/modules/customer/components/customers/form-customer/form-customer.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Customer } from '@modules/customer/customer.model';
 
 @Component({
   selector: 'app-form-customer',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormCustomerComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.buildForm();
   }
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      name: ["", Validators.required],
+      phone: [""],
+      email: [""],
+      address: [""],
+      group_ids: [""]
+    });
+  }
+
+  setValueForm(customer: Customer) {
+    this.resetForm();
+    this.form.patchValue({
+      name: customer.name,
+      phone: customer.phone,
+      email: customer.email,
+      address: customer.address,
+      group_ids: customer.groups.map(g => g.id)
+    })
+  }
+
+  resetForm() {
+    this.form.reset();
+  }
+
+  getValueForm() { 
+    return this.form.value;
+  }
+
+  
 
 }
