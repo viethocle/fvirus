3181199711c51aad7dbad533c324d238953f9627
minhthuan <doanminhthuan274@gmail.com>
2018-05-07 10:45:00 +0700

    destroyable detail order

--- src/app/modules/dashboard/components/detail-order/detail-order.component.ts
+++ src/app/modules/dashboard/components/detail-order/detail-order.component.ts
@@ -1,32 +1,13 @@
-import { RoleUser, AuthService } from './../../../auth/auth.service';
-import { BsModalComponent } from 'ng2-bs3-modal';
-import { DashboardService } from '../../dashboard.service';
-import { PerfectScrollbarComponent } from 'ngx-perfect-scrollbar';
-import { tap } from 'rxjs/operators';
-import { CustomerService } from './../../../customer/customer.service';
-import { Order } from './../../order';
-import {
-  Component,
-  OnInit, OnDestroy,
-  ViewChild, Output, EventEmitter, ViewChildren, ElementRef, Renderer2,
-  AfterViewChecked,
-  ChangeDetectorRef
-} from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  Validators,
-  FormBuilder,
-  AbstractControl,
-  FormArray
-} from '@angular/forms';
-import { Customer } from "@modules/customer/customer.model";
-import { FlyInOut } from '../../flyInOut.animate';
-import * as _ from 'lodash';
-import { Destroyable, takeUntilDestroy } from 'take-until-destroy'
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { BsmodalService } from '@core/services/bsmodal.service';
-import createNumberMask from 'text-mask-addons/dist/createNumberMask';
+import { BsModalComponent } from 'ng2-bs3-modal';
 
+import { DashboardService } from '../../dashboard.service';
+import { AuthService } from './../../../auth/auth.service';
+import { Order } from './../../order';
+import { Destroyable, takeUntilDestroy } from 'take-until-destroy'
+
+@Destroyable
 @Component({
   selector: 'app-detail-order',
   templateUrl: './detail-order.component.html',
@@ -44,12 +25,14 @@ export class DetailOrderComponent implements OnInit {
 
   ngOnInit() {
     this.bsmodalService.orderDetail$
+        .pipe(
+          takeUntilDestroy(this)
+        )
         .subscribe(order => {
           if (order) {
             this.order = order;
             this.contents = JSON.parse(order.contents);
             this.modalDetail.open();
-            console.log(order);
           }
         });
   }
