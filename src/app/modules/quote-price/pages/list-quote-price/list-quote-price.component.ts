e33bc0105dcc7033c80501822e2f16e57984d98b
minhthuan <doanminhthuan274@gmail.com>
2018-05-18 10:41:41 +0700

    open modal with dynamic component

--- src/app/modules/quote-price/pages/list-quote-price/list-quote-price.component.ts
+++ src/app/modules/quote-price/pages/list-quote-price/list-quote-price.component.ts
@@ -1,16 +1,30 @@
+import { TemplateQuotePriceComponent } from './../../components/template-quote-price/template-quote-price.component';
+import { Store, select } from '@ngrx/store';
+import { AdDirective } from './../../directives/ad.directive';
+import { BsModalComponent } from 'ng2-bs3-modal';
 import { QuoteService } from './../../quote.service';
 import { QuotePrice } from './../../quote-price.model';
 import { Subject } from 'rxjs/Subject';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ComponentFactoryResolver } from '@angular/core';
 import { Router } from '@angular/router';
 import * as _ from 'lodash';
+import { Destroyable, takeUntilDestroy } from "take-until-destroy";
+import * as quoteDataActions from '../../quote-data';
 
+
+interface QuoteState {
+  data: any;
+}
+
+@Destroyable
 @Component({
   selector: 'app-list-quote-price',
   templateUrl: './list-quote-price.component.html',
   styleUrls: ['./list-quote-price.component.css']
 })
 export class ListQuotePriceComponent implements OnInit {
+  @ViewChild("modal") modal: BsModalComponent;
+  @ViewChild(AdDirective) adHost: AdDirective;
 
   dtOptions: DataTables.Settings = {};
   dtTrigger: Subject<any> = new Subject();
@@ -19,7 +33,9 @@ export class ListQuotePriceComponent implements OnInit {
   
   constructor(
     private quoteService: QuoteService,
-    private router: Router 
+    private router: Router,
+    private store: Store<QuoteState>,
+    private componentFactoryResolver: ComponentFactoryResolver
   ) { }
 
   ngOnInit() {
@@ -31,7 +47,11 @@ export class ListQuotePriceComponent implements OnInit {
       }
     };
 
-    this.quoteService.getQuotePrices().subscribe(res => { 
+    this.quoteService.getQuotePrices()
+      .pipe(
+        takeUntilDestroy(this)
+      )
+      .subscribe(res => { 
       this.quote_prices = res; 
       this.dtTrigger.next();
     });
@@ -41,6 +61,18 @@ export class ListQuotePriceComponent implements OnInit {
     if (_.isNumber(customer_id)) {
       this.router.navigate(["/customers", customer_id]);
     }
+  }
+
+  openPreview(quote: QuotePrice) {
+    this.store.dispatch(new quoteDataActions.Store(quote.value));
+    let componentFactory = this.componentFactoryResolver.resolveComponentFactory(TemplateQuotePriceComponent);
+
+    let viewContainerRef = this.adHost.viewContainerRef;
+    viewContainerRef.clear();
+
+    let componentRef = viewContainerRef.createComponent(componentFactory);
+
+    this.modal.open();
   }
 
 
